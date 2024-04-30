d9233628b2937b2953dc1e62e1fe804dcf50f42e
shubhangi212001 <shubhangishinde820@gmail.com>
2024-04-30 19:44:27 +0530

    changes

--- sca_vite/vite.config.js
+++ sca_vite/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   hot: true,
-  //open: true,
+  open: true,
   server: {
     port: 3000,
-    open: true,
+    //open: false,
     host: '0.0.0.0',
 
 },
